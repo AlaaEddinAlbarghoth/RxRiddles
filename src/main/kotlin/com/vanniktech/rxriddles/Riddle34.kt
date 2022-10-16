f1b062fc7ea5ca94be6f49d97352e5bd8423f248
Alaa Eddin Albarghoth <eng.alaa.brghoth@gmail.com>
2022-10-16 18:06:22 +0300

    Riddle34 Solution

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle34.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle34.kt
@@ -4,12 +4,12 @@ import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Scheduler
 
 object Riddle34 {
-  /**
-   * Shift all downstream and upstream work of [source] to the given [scheduler].
-   *
-   * Use case: You want to shift work to a particular [Scheduler].
-   */
-  fun solve(source: Completable, scheduler: Scheduler): Completable {
-    TODO()
-  }
+    /**
+     * Shift all downstream and upstream work of [source] to the given [scheduler].
+     *
+     * Use case: You want to shift work to a particular [Scheduler].
+     */
+    fun solve(source: Completable, scheduler: Scheduler): Completable {
+        return source.subscribeOn(scheduler)
+    }
 }
