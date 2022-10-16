0b0ba2e68e421389d9eb71b1771dc5c2bfb6ab1b
Alaa Eddin Albarghoth <eng.alaa.brghoth@gmail.com>
2022-10-16 16:50:13 +0300

    Riddle28 Solution

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle28.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle28.kt
@@ -3,12 +3,12 @@ package com.vanniktech.rxriddles
 import io.reactivex.rxjava3.core.Completable
 
 object Riddle28 {
-  /**
-   * Call the given [function] when the [source] completes.
-   *
-   * Use case: Add some logging.
-   */
-  fun solve(source: Completable, function: () -> Unit): Completable {
-    TODO()
-  }
+    /**
+     * Call the given [function] when the [source] completes.
+     *
+     * Use case: Add some logging.
+     */
+    fun solve(source: Completable, function: () -> Unit): Completable {
+        return source.doOnComplete(function)
+    }
 }
